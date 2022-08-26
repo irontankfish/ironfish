c4e51cdc9817f708dc2012c8b03caf281ea15146
Mat <mat@ironfish.network>
2022-08-26 09:19:56 -0700

    Revert to napi 2.2 since that's what we're using, add object benchmark

--- ironfish/src/playground.ts
+++ ironfish/src/playground.ts
@@ -3,6 +3,7 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import {
   Foo,
+  FooObj,
   randomBytes,
   randomBytesBuffer,
   randomBytesString,
@@ -27,16 +28,50 @@ const withSegment = async (title: string, fn: () => Promise<void> | void): Promi
 
 class JsFoo {}
 
-async function testBoxKeyPair() {
+async function testObject() {
   console.log('is gc?', global.gc != null)
-  const test_counts = [1, 25, 10_000, 250_000, 1_000_000]
+  const test_counts = [1, 25, 10_000, 250_000, 1_000_000, 5_000_000]
 
   const results: string[] = []
-  // const bufList = []
 
   const overallSegment = BenchUtils.startSegment()
 
-  await pauseAndGc(1000)
+  await pauseAndGc(10)
+
+  for (const TEST_COUNT of test_counts) {
+    const result = await withSegment(
+      `BoxKeyPair - ${TEST_COUNT.toLocaleString()} iterations`,
+      async () => {
+        for (let i = 0; i < TEST_COUNT; i += 1) {
+          // new JsFoo()
+          const x = {} as FooObj
+          if (i % 5000 === 0) {
+            await PromiseUtils.sleep(10)
+          }
+        }
+        await pauseAndGc(10)
+      },
+    )
+    results.push(result)
+
+    await pauseAndGc(10)
+  }
+
+  const endOverall = BenchUtils.endSegment(overallSegment)
+  results.push(BenchUtils.renderSegment(endOverall, 'Overall', '\n\t'))
+
+  console.log(results.join('\n'))
+}
+
+async function testBoxKeyPair() {
+  console.log('is gc?', global.gc != null)
+  const test_counts = [1, 25, 10_000, 250_000, 1_000_000, 5_000_000]
+
+  const results: string[] = []
+
+  const overallSegment = BenchUtils.startSegment()
+
+  await pauseAndGc(10)
 
   for (const TEST_COUNT of test_counts) {
     const result = await withSegment(
@@ -45,30 +80,17 @@ async function testBoxKeyPair() {
         for (let i = 0; i < TEST_COUNT; i += 1) {
           // new JsFoo()
           new Foo()
-          // const x = new JsFoo()
-          // if (i == 5) {
-          //   bufList.push(x)
-          // }
-          if (i % (TEST_COUNT / 10) === 0) {
-            // await PromiseUtils.sleep(1000)
-            await pauseAndGc(1000)
+          if (i % 5000 === 0) {
+            await PromiseUtils.sleep(10)
           }
         }
-        await pauseAndGc(2000)
-        await pauseAndGc(2000)
-        await pauseAndGc(2000)
+        await pauseAndGc(10)
       },
     )
     results.push(result)
 
-    await pauseAndGc(1000)
+    await pauseAndGc(10)
   }
-
-  await pauseAndGc(1000)
-  await pauseAndGc(2000)
-  await pauseAndGc(4000)
-  await pauseAndGc(4000)
-  await pauseAndGc(5000)
 
   const endOverall = BenchUtils.endSegment(overallSegment)
   results.push(BenchUtils.renderSegment(endOverall, 'Overall', '\n\t'))
@@ -77,11 +99,7 @@ async function testBoxKeyPair() {
 }
 
 async function testRandomBytes(test_count: number) {
-  // const TEST_COUNT = 2_000_000
-  // const TEST_COUNT = 50_000
-
   const results = [`Number of iterations: ${test_count.toLocaleString()}`]
-  const bufList = []
 
   const tests = [
     {
@@ -107,43 +125,31 @@ async function testRandomBytes(test_count: number) {
   ]
   tests.sort(() => Math.random() - 0.5)
 
-  await pauseAndGc(1000)
+  await pauseAndGc(10)
 
   for (const test of tests) {
     const result = await withSegment(test.title, async () => {
       for (let i = 0; i < test_count; i += 1) {
-        const x = test.fn(32)
-        if (i == 5) {
-          bufList.push(x)
-        }
+        test.fn(32)
         if (i % 5000 === 0) {
-          await pauseAndGc(10)
-          // await PromiseUtils.sleep(1000)
+          await PromiseUtils.sleep(10)
         }
       }
-      await pauseAndGc(500)
-      await pauseAndGc(1000)
-      await pauseAndGc(4000)
+      await pauseAndGc(10)
     })
 
     results.push(result)
   }
 
   console.log(results.join('\n'))
-  await pauseAndGc(1000)
 }
-// testBoxKeyPair()
 
 async function main() {
-  await testRandomBytes(1)
-  await pauseAndGc(1000)
-  await testRandomBytes(25)
-  await pauseAndGc(1000)
+  // await testObject()
+  // await testBoxKeyPair()
   await testRandomBytes(1_000_000)
-  await pauseAndGc(1000)
+  await pauseAndGc(100)
   await testRandomBytes(5_000_000)
-  await pauseAndGc(1000)
-  await testRandomBytes(20_000_000)
 }
 
 void main()
